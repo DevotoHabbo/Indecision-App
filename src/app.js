5f0fe67373d98f8d0373d3f1a7a2fa278b33c652
Nguyen Phuong <nguyenphuong@Nguyens-MacBook-Pro.local>
2020-03-21 21:29:20 -0400

    deployable version

--- src/app.js
+++ src/app.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 import'normalize.css/normalize.css'
 import '../src/styles/styles.scss'
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env || 3000
 
-
+app.get('/', (req,res)=>{ res.sendfile(path.join(__dirname + '/index.html'))})
+app.listen(port,()=>{console.log(`Server connected on port ${port}!`)})
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
